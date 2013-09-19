fd36f49031d8d17da8aeb0d25eeb2f409ab95521
Mike Verdone <mike.verdone@gmail.com>
2013-09-19 21:52:08 +0200

    Does nothing yet.

---
+++ src/weirdPlayer/main.js
@@ -0,0 +1,11 @@
+;window.weirdPlayer = window.weirdPlayer || {};
+(function (window) {
+    var exports = {};
+
+    function setup(playerNode, apiUrl) {
+
+    }
+    exports.setup = setup;
+
+    window.weirdPlayer.main = exports;
+})(window);
