3ad43f17c95ff6b9320c5aae971f742a09da4004
Mike Verdone <mike.verdone@gmail.com>
2013-09-19 22:20:16 +0200

    Nothing much yet.

--- src/weirdPlayer/main.js
+++ src/weirdPlayer/main.js
@@ -1,9 +1,33 @@
 ;window.weirdPlayer = window.weirdPlayer || {};
 (function (window) {
+    "use strict";
     var exports = {};
 
-    function setup(playerNode, apiUrl) {
+    function wpApiParamStr(params) {
+        var pStr = "?json=1";
+        for (var key in params) {
+            // yup, this seems safe...
+            pStr += "&" + key + "=" + params[key];
+        }
+        return pStr;
+    }
 
+    function load(apiUrl, params, okCallback, errCallback) {
+        var pStr = wpApiParamStr(params),
+            req = new window.XMLHttpRequest();
+        req.onload  = okCallback .bind(undefined, req);
+        req.onerror = errCallback.bind(undefined, req);
+        req.open("GET", apiUrl + pStr, true);
+    }
+
+    function setupPlayerNode(playerNode) {
+        
+    }
+
+    function WeirdPlayer(playerNode, apiUrl) {
+    }
+    function setup(playerNode, apiUrl) {
+        new WeirdPlayer(playerNode, apiUrl);
     }
     exports.setup = setup;
 
