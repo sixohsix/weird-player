f8cf0e2c03918be765275ce97a169de5e580f044
Mike Verdone <mike.verdone@gmail.com>
2013-10-03 15:10:31 +0200

    Parse artist and release from title.

--- src/weirdPlayer/parse.js
+++ src/weirdPlayer/parse.js
@@ -1,16 +1,23 @@
 ;window.weirdPlayer = window.weirdPlayer || {};
 window.weirdPlayer.parse = (function (window) {
     "use strict";
-    var exports = {};
+    var exports = {},
+        util    = window.weirdPlayer.util,
+        defined = util.defined;
 
-    function nodeListToArray(nl) {
+    function singlePost(jsonResponse) {
+        return jsonResponse.posts[0];
+    }
+    exports.singlePost = singlePost;
+
+    function nodeListToArray(nl) { // NodeList -> Array
         var arr = [];
         for (var n of nl) arr.push(n);
         return arr;
     }
 
-    function htmlContent(jsonResponse) { // WpJsonResponse -> HTMLDivElement
-        var htmlStr = jsonResponse.posts[0].content,
+    function htmlContent(post) { // WpJsonResponse -> HTMLDivElement
+        var htmlStr = post.content,
             div     = document.createElement("div");
         div.innerHTML = htmlStr;
         return div;
@@ -29,6 +36,20 @@ window.weirdPlayer.parse = (function (window) {
         return sourceLists;
     }
     exports.parseAudioNodes = parseAudioNodes;
+
+    var titleRe = new window.RegExp("^New Canadiana :: (.+) &#8211; (.+)$");
+
+    function parseArtistData(post) { // WpPost -> {artist, release}|undefined
+        if (! defined(post.title)) return undefined;
+
+        var title = post.title,
+            match = titleRe.exec(title);
+
+        if (match === null) return undefined;
+
+        return {artist: match[1], release: match[2]};
+    };
+    exports.parseArtistData = parseArtistData;
 
     return exports;
 })(window);
--- tests/parse.js
+++ tests/parse.js
@@ -6,9 +6,14 @@
         util    = window.weirdPlayer.util,
         defined = util.defined,
 
-        parse   = window.weirdPlayer.parse,
+        parse      = window.weirdPlayer.parse,
+        singlePost = parse.singlePost,
 
-        fixture = window.tests.fixture;
+        fixture = window.tests.fixture,
+
+        post    = singlePost(fixture.aJsonResponse);
+
+    window.QUnit.module("parse");
 
     test("The test system is running", function () {
         ok(true, "yup, it works.");
@@ -16,16 +21,24 @@
     });
 
     test("parse html from a json response", function () {
-        var html = parse.htmlContent(fixture.aJsonResponse);
+        var html = parse.htmlContent(post);
         ok(defined(html), "html defined");
     });
 
     test("parse audio nodes from json response", function () {
-        var el          = parse.htmlContent(fixture.aJsonResponse),
+        var el          = parse.htmlContent(post),
             sourceLists = parse.parseAudioNodes(el);
         eq(sourceLists.length, 2, "two nodes were parsed");
         eq(sourceLists[1][1].src, "http://weirdcanada.com/binary/Weird_Canada-Nouveau_Zodiaque-Combustible.mp3",
            "got the right src");
+    });
+
+
+    test("parse artist and release from post title", function () {
+        var postData = parse.parseArtistData(post);
+        ok(defined(postData), "postData came out");
+        eq(postData.artist, "Nouveau Zodiaque");
+        eq(postData.release, "Nouveau Zodiaque EP");
     });
 
 })(window);
