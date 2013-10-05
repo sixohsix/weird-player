3fbaf5c2e0ee6ac696faf0658d5f313b2eeac471
Mike Verdone <mike.verdone@gmail.com>
2013-10-05 17:25:37 +0200

    Don't forget css.

--- makedist.sh
+++ makedist.sh
@@ -4,6 +4,6 @@ rm -rf dist
 mkdir -p dist
 ./scrunchItUp.py
 cp wcp.js dist/
-cp html/wclogo.jpg dist
+cp html/wclogo.jpg dist/
+cp html/player.css dist/
 cp html/player-packed.html dist/index.html
-
